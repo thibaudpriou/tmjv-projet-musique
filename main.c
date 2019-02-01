a82e93adfbf2af0ec92e7826e6adc46396308125
Thibaud Priou <thibaud.priou@gmail.com>
2019-02-01 19:41:51 +0100

    Clean code

--- main.c
+++ main.c
@@ -32,40 +32,35 @@ main(int argc, char * argv [])
 
   progname = strrchr(argv[0], '/');
   progname = progname ? progname + 1 : argv[0];
+	if(argc != 3) {
 
-  if(argc != 3)
-  {
     print_usage(progname);
     return 1;
-  };
+  }
 
   infilename = argv[1];
   outfilename = argv[2];
 
-  if(strcmp(infilename, outfilename) == 0)
-  {
+	if(strcmp(infilename, outfilename) == 0) {
     printf("Error : Input and output filenames are the same.\n\n");
     print_usage(progname);
     return 1;
-  };
+  }
 
-
-  if((infile = sf_open(infilename, SFM_READ, &sfinfo)) == NULL)
-  {
+  if((infile = sf_open(infilename, SFM_READ, &sfinfo)) == NULL) {
     printf("Not able to open input file %s.\n", infilename);
     puts(sf_strerror(NULL));
     return 1;
-  };
+  }
 
   int nb_frames_in =(int)sfinfo.frames/HOP_SIZE;
 
   /* Open the output file. */
-  if((outfile = sf_open(outfilename, SFM_WRITE, &sfinfo)) == NULL)
-  {
+  if((outfile = sf_open(outfilename, SFM_WRITE, &sfinfo)) == NULL) {
     printf("Not able to open input file %s.\n", outfilename);
     puts(sf_strerror(NULL));
     return 1;
-  };
+  }
 
   /* Read WAV */
   int nb_frames = 0;
@@ -73,15 +68,15 @@ main(int argc, char * argv [])
   double buffer[FRAME_SIZE];
 
   int i;
-  for(i=0;i<(FRAME_SIZE/HOP_SIZE-1);i++)
-  {
-    if(read_samples(infile, new_buffer, sfinfo.channels)==1) {
-        fill_buffer(buffer, new_buffer);
+  for(i=0; i<(FRAME_SIZE/HOP_SIZE-1); i++) {
+    if(read_samples(infile, new_buffer, sfinfo.channels) == 1) {
+			fill_buffer(buffer, new_buffer);
     } else {
       printf("not enough samples !!\n");
       return 1;
     }
   }
+
 
   complex samples[FRAME_SIZE];
   double amplitude[FRAME_SIZE];
@@ -90,19 +85,18 @@ main(int argc, char * argv [])
   double spectralFlux[nb_frames_in];
   double FS = 0.0;
 
-  for(i = 0; i < FRAME_SIZE; i++){
+  for(i = 0; i < FRAME_SIZE; i++) {
       amplitude_prev[i] = 0.0;
   }
 
   /* Plot init */
-  h=gnuplot_init();
+  h = gnuplot_init();
   gnuplot_setstyle(h, "lines");
 
   /* FFT init */
   plan = fft_init(samples, spec);
 
-  while(read_samples(infile, new_buffer, sfinfo.channels)==1)
-  {
+  while(read_samples(infile, new_buffer, sfinfo.channels) == 1) {
     /* Process Samples */
     printf("Processing frame %d\n",nb_frames);
 
@@ -110,15 +104,14 @@ main(int argc, char * argv [])
     fill_buffer(buffer, new_buffer);
 
     // fft input
-    for(i = 0; i < FRAME_SIZE; i++){
+    for(i = 0; i < FRAME_SIZE; i++) {
         samples[i] = buffer[i];
     }
 
     fft_process(plan);
 
     // amplitude
-    for(i = 0; i < FRAME_SIZE; i++)
-    {
+    for(i = 0; i < FRAME_SIZE; i++) {
       amplitude_prev[i] = amplitude[i];
       amplitude[i] = cabs(spec[i]);
     }
@@ -126,25 +119,23 @@ main(int argc, char * argv [])
     /* SPECTRAL FLUX !!! */
     FS = 0.0;
 
-    for(i = 0; i < FRAME_SIZE; i++)
-    {
+    for(i = 0; i < FRAME_SIZE; i++) {
       double tmp = amplitude[i] - amplitude_prev[i];
-      if(tmp > 0){
+      if(tmp > 0) {
           FS += tmp;
       }
     }
 
-    FS /=(double)FRAME_SIZE;
+    FS /= (double)FRAME_SIZE;
 
     /* threshold detection */
     if(FS > 0.3) {
       spectralFlux[nb_frames] = FS;
 
       /* sin synthesis */
-      for(i = 0; i < FRAME_SIZE; i++)
-      {
+      for(i = 0; i < FRAME_SIZE; i++) {
         buffer[i] +=(0.25*sin(2*M_PI*1760.0*(double)i/sfinfo.samplerate)) *
-       (0.5-0.5*cos(2.0*M_PI*(double)i/FRAME_SIZE));;
+       			(0.5-0.5*cos(2.0*M_PI*(double)i/FRAME_SIZE));;
       }
     } else {
       spectralFlux[nb_frames] = 0.0;
@@ -154,7 +145,6 @@ main(int argc, char * argv [])
     // if(write_samples(outfile, buffer, sfinfo.channels)!=1){
     //   printf("saving problem !! \n");
     // }
-
 
     nb_frames++;
   }
@@ -180,37 +170,35 @@ main(int argc, char * argv [])
   sleep(20);
   */
 
-
   /* ---------------- */
   // Calcul du dephasage
   double peigne[nb_frames_in];
-  for(int i=0; i < nb_frames_in; i++)
-  {
-    if(i % imax == 0)
-    peigne[i] = 1;
-    else
-    peigne[i] = 0;
+  for(int i=0; i < nb_frames_in; i++) {
+    if(i % imax == 0) {
+    	peigne[i] = 1;
+    } else{
+    	peigne[i] = 0;
+		}
   }
 
   double gamma[nb_frames_in];
-  for(int tau = 0; tau < nb_frames_in; tau++)
-  {
+  for(int tau = 0; tau < nb_frames_in; tau++) {
     gamma[tau]=0;
-    for(int i = 0; i < nb_frames_in-tau; i++)
-    gamma[tau] +=(spectralFlux[i]*peigne[i+tau])/(double)nb_frames_in;
-    //gamma[tau]/=(1.0/nb_frames);
+    for(int i = 0; i < nb_frames_in-tau; i++) {
+	    gamma[tau] +=(spectralFlux[i]*peigne[i+tau])/(double)nb_frames_in;
+	    //gamma[tau]/=(1.0/nb_frames);
+		}
   }
 
   int peigne_imax=0;
   double peigne_max = 0.0;
 
-  for(int i=0; i < imax; i++)
-  if(gamma[i] > peigne_max)
-  {
-    peigne_max = gamma[i];
-    peigne_imax = i;
-  }
-
+  for(int i=0; i < imax; i++){
+	  if(gamma[i] > peigne_max) {
+	    peigne_max = gamma[i];
+	    peigne_imax = i;
+	  }
+	}
 
   printf("max intercorrelation %d\n",peigne_imax);
   /* PLOT */
@@ -221,58 +209,20 @@ main(int argc, char * argv [])
   sf_close(infile);
 
   /* Synthesis beat tracker */
-  if((infile = sf_open(infilename, SFM_READ, &sfinfo)) == NULL)
-  {
+  if((infile = sf_open(infilename, SFM_READ, &sfinfo)) == NULL) {
     printf("Not able to open input file %s.\n", infilename);
     puts(sf_strerror(NULL));
     return 1;
-  };
+  }
 
-  /* Open the output file. */
-  if((outfile = sf_open(outfilename, SFM_WRITE, &sfinfo)) == NULL)
-  {
-    printf("Not able to open input file %s.\n", outfilename);
-    puts(sf_strerror(NULL));
-    return 1;
-  };
-
-  for(i=0;i<(FRAME_SIZE/HOP_SIZE-1);i++)
-  {
-    if(read_samples(infile, new_buffer, sfinfo.channels)==1)
-    fill_buffer(buffer, new_buffer);
-    else
-    {
+  for(i=0; i<(FRAME_SIZE/HOP_SIZE-1); i++) {
+    if(read_samples(infile, new_buffer, sfinfo.channels)==1){
+	    fill_buffer(buffer, new_buffer);
+    } else {
       printf("not enough samples !!\n");
       return 1;
     }
   }
-
-  // nb_frames=0;
-  // while(read_samples(infile, new_buffer, sfinfo.channels)==1)
-  // {
-  //   /* Process Samples */
-  //   printf("Step 2 : Processing frame %d\n",nb_frames);
-  //   /* hop size */
-  //   fill_buffer(buffer, new_buffer);
-	//
-  //   if(nb_frames % imax ==(imax - peigne_imax)) /* beat located */
-  //   {
-  //     /* sin synthesis */
-	//
-  //     for(i = 0; i < FRAME_SIZE; i++)
-  //     {
-  //       buffer[i] +=(0.5*sin(2*M_PI*1760.0*(double)i/sfinfo.samplerate)) *
-  //      (0.5-0.5*cos(2.0*M_PI*(double)i/FRAME_SIZE));;
-  //     }
-  //   }
-	//
-  //   /* SAVE */
-  //   if(write_samples(outfile, buffer, sfinfo.channels)!=1){
-  //     printf("saving problem !! \n");
-  //   }
-  //   nb_frames++;
-  // }
-
 
   sf_close(infile);
   sf_close(outfile);
--- rythme.c
+++ rythme.c
@@ -1,23 +1,26 @@
 #include "rythme.h"
 
-int getTempoEstimation(int nb_frames, double *data){
+int getTempoEstimation(int nb_frames, double *data)
+{
 	double gamma[nb_frames];
 
-  for(int tau = 0; tau < nb_frames; tau++)
-  {
-    gamma[tau]=0;
-    for(int i = 0; i < nb_frames-tau; i++)
-    gamma[tau] +=(data[i]*data[i+tau])/(double)nb_frames;
+  for(int tau = 0; tau < nb_frames; tau++) {
+    gamma[tau] = 0;
+    for(int i = 0; i < nb_frames-tau; i++){
+      gamma[tau] += (data[i]*data[i+tau])/(double)nb_frames;
+    }
   }
 
   int imax=0;
   double autoc_max = 0.0;
 
-  for(int i=20; i < 200; i++) //nb_frames //200 pour NiN
-  if(gamma[i] > autoc_max)
-  {
-    autoc_max = gamma[i];
-    imax = i;
+  //nb_frames //200 pour NiN
+  for(int i=20; i < 200; i++) {
+    if(gamma[i] > autoc_max) {
+      autoc_max = gamma[i];
+      imax = i;
+    }
   }
+
 	return imax;
 }
